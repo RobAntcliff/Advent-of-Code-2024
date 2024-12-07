413b42f4d01e89a3d0ad66fef4434c5f0ce62c82
Robert Antcliff <robertantcliff@gmail.com>
2024-12-07 12:34:12 +0000

    Day 7 solution

---
+++ day_7/day_7.rb
@@ -0,0 +1,58 @@
+class Day7
+    def part_1
+        result = 0
+        File.open("day_7_input.txt").each do |line|
+            input_array = line.chomp.split(" ")
+            target_number = input_array[0].to_i
+            numbers = input_array[1..-1].map(&:to_i)
+            operations = operations = ['+', '*']
+            result += target_number if number_can_be_created(target_number, numbers, operations)
+        end
+        puts result
+    end
+
+    def part_2
+        result = 0
+        File.open("day_7_input.txt").each do |line|
+            input_array = line.chomp.split(" ")
+            target_number = input_array[0].to_i
+            numbers = input_array[1..-1].map(&:to_i)
+            operations = operations = ['+', '*', '||']
+            result += target_number if number_can_be_created(target_number, numbers, operations)
+            puts line
+        end
+        puts result
+    end
+
+    def number_can_be_created(target_number, numbers, operations)
+        operations_combinations = generate_all_operation_combinations(numbers.length - 1, operations)
+        operations_combinations.each do |operations_array|
+            return true if is_valid_expression(target_number, numbers, operations_array)
+        end
+        false
+    end
+
+    def is_valid_expression(target_number, numbers, operations_array)
+        to_check = numbers[0]
+        operations_array.each_with_index do |to_operate, index3|
+            if to_operate == '||'
+                to_check = (to_check.to_s + numbers[index3 + 1].to_s).to_i
+            elsif to_operate == '+'
+                to_check += numbers[index3 + 1]
+            else
+                to_check *= numbers[index3 + 1]
+            end
+        end
+        return true if to_check == target_number
+        false
+    end
+
+    def generate_all_operation_combinations(length, operations)
+        operations.repeated_permutation(length).to_a
+    end
+
+end
+
+day7 = Day7.new
+# day7.part_1
+day7.part_2
