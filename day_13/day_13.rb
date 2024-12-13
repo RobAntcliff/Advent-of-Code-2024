35c3a338b0da9327fad07b239027171f388d30bf
Robert Antcliff <robertantcliff@gmail.com>
2024-12-13 12:42:00 +0000

    Day 13 Golf

--- day_13/day_13.rb
+++ day_13/day_13.rb
@@ -1,139 +1,36 @@
-def part_1
-  # Read and parse input file
-  input = File.read(File.join(__dir__, "day_13_input.txt")).split("\n\n")
-  
-  # Process each block of coordinates
-  coordinate_blocks = input.map do |block|
-    lines = block.split("\n")
-
-    x1 = lines[0].split("X+")[1].split(",")[0].to_i
-    y1 = lines[0].split("Y+")[1].to_i
-
-    x2 = lines[1].split("X+")[1].split(",")[0].to_i
-    y2 = lines[1].split("Y+")[1].to_i
-
-    prize_x = lines[2].split("X=")[1].split(",")[0].to_i
-    prize_y = lines[2].split("Y=")[1].to_i
-    
-    {
-      button_a: { x: x1.to_i, y: y1.to_i },
-      button_b: { x: x2.to_i, y: y2.to_i },
-      prize: { x: prize_x.to_i, y: prize_y.to_i }
-    }
-  end
- 
+def calc_cost(part)
+  equations = parse_input("day_13_input.txt")
   result = 0
-  coordinate_blocks.each do |block|
-    # Equation 1: x1 * y1 = e1
-    x1 = block[:button_a][:x]
-    y1 = block[:button_b][:x]
-    e1 = block[:prize][:x]
-
-    # Equation 2: x2 * y2 = e2
-    x2 = block[:button_a][:y]
-    y2 = block[:button_b][:y]
-    e2 = block[:prize][:y]
-    
-    # Mult first equation by y from the second equation to eliminate y
-    new_x1 = x1 * y2
-    new_y1 = y1 * y2
-    new_e1 = e1 * y2
-
-    # mult second equation by y from the first equation
-    new_x2 = x2 * y1
-    new_y2 = y2 * y1
-    new_e2 = e2 * y1
-
-    # Eliminated y
-    x3 = new_x1 - new_x2
-    e3 = new_e1 - new_e2
-
-    if(e3 % x3 != 0)
-      next
+  limit = part == 1 ? 100 : nil
+  equations.each do |e|
+    if(part == 2)
+      e[:e1] += 10000000000000
+      e[:e2] += 10000000000000
     end
-
-    x = e3 / x3
-
-    if((e1 - (x1 * x)) % y1 != 0)
-      next
-    end
-
-    y = (e1 - (x1 * x)) / y1
-
-    if(x <= 100 && y <= 100)
-      result += (x * 3) + y
-    end
+    temp_result = do_algebra(e[:x1], e[:y1],e[:e1], e[:x2], e[:y2], e[:e2], limit)
+    result += temp_result if temp_result
   end
-
-  puts result
+  return result
 end
 
-def part_2
-  # Read and parse input file
-  input = File.read(File.join(__dir__, "day_13_input.txt")).split("\n\n")
-  
-  # Process each block of coordinates
-  coordinate_blocks = input.map do |block|
-    lines = block.split("\n")
-
-    x1 = lines[0].split("X+")[1].split(",")[0].to_i
-    y1 = lines[0].split("Y+")[1].to_i
-
-    x2 = lines[1].split("X+")[1].split(",")[0].to_i
-    y2 = lines[1].split("Y+")[1].to_i
-
-    prize_x = lines[2].split("X=")[1].split(",")[0].to_i + 10000000000000
-    prize_y = lines[2].split("Y=")[1].to_i + 10000000000000
-    
-    {
-      button_a: { x: x1.to_i, y: y1.to_i },
-      button_b: { x: x2.to_i, y: y2.to_i },
-      prize: { x: prize_x.to_i, y: prize_y.to_i }
-    }
+def do_algebra(x1, y1, e1, x2, y2, e2, limit = nil)
+  x = ((e1 * y2) - (e2 * y1)) / ((x1 * y2) - (x2 * y1))
+  y = (e1 - (x1 * x)) / y1
+  if(((e1 * y2) - (e2 * y1)) % ((x1 * y2) - (x2 * y1)) != 0 || ((e1 - (x1 * x)) % y1 != 0) || (limit && (x > limit || y > limit)))
+    return nil
   end
- 
-  result = 0
-  coordinate_blocks.each do |block|
-    # Equation 1: x1 * y1 = e1
-    x1 = block[:button_a][:x]
-    y1 = block[:button_b][:x]
-    e1 = block[:prize][:x]
-
-    # Equation 2: x2 * y2 = e2
-    x2 = block[:button_a][:y]
-    y2 = block[:button_b][:y]
-    e2 = block[:prize][:y]
-    
-    # Mult first equation by y from the second equation to eliminate y
-    new_x1 = x1 * y2
-    new_y1 = y1 * y2
-    new_e1 = e1 * y2
-
-    # mult second equation by y from the first equation
-    new_x2 = x2 * y1
-    new_y2 = y2 * y1
-    new_e2 = e2 * y1
-
-    # Eliminated y
-    x3 = new_x1 - new_x2
-    e3 = new_e1 - new_e2
-
-    if(e3 % x3 != 0)
-      next
-    end
-
-    x = e3 / x3
-
-    if((e1 - (x1 * x)) % y1 != 0)
-      next
-    end
-
-    y = (e1 - (x1 * x)) / y1
-
-    result += (x * 3) + y
-  end
-
-  puts result
+  return (x * 3) + y
 end
 
-part_2
+def parse_input(input_file)
+  return File.read(File.join(__dir__, input_file)).split("\n\n").map { |b| l = b.split("\n"); { x1: l[0].split("X+")[1].split(",")[0].to_i, x2: l[0].split("Y+")[1].to_i, y1: l[1].split("X+")[1].split(",")[0].to_i, y2: l[1].split("Y+")[1].to_i, e1: l[2].split("X=")[1].split(",")[0].to_i, e2: l[2].split("Y=")[1].to_i } }
+end
+
+start_time = Time.now
+puts "Part 1 Result: #{calc_cost(1)}"
+puts "Time taken to run part 1: #{Time.now - start_time} seconds"
+
+start_time = Time.now
+puts "\nPart 2 Result: #{calc_cost(2)}"
+puts "Time taken to run part 2: #{Time.now - start_time} seconds"
+  
